0561d3069f5c46e30e23a6deac68a49a33134987
michal <kuk.michal3@gmail.com>
2021-01-15 11:43:51 +0100

    add unit tests

--- src/test/java/Nokia/BasketTest.java
+++ src/test/java/Nokia/BasketTest.java
@@ -10,7 +10,7 @@ public class BasketTest {
     public void should_check_if_summarize_all_purchase(){
         //given
         Basket basket = new Basket();
-        Item bike = new Item("bike", 20);
+        Item bike = new Item("random bike", 20);
         Item lamp = new Item("lamp", 9);
         basket.addItem(bike, lamp);
 
